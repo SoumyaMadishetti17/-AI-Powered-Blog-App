2ad30472a742e96f92363679d09fa0d809e1ea7a
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2025-07-11 12:22:39 +0530

    stage-admin-dashboard

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>      
-      <div className="text-4xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-400 to-indigo-500 text-transparent bg-clip-text">
+      <div onClick={()=>navigate('/')} className="text-4xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-400 to-indigo-500 text-transparent bg-clip-text">
         DevBlogs
       </div>
       <button onClick={()=> navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Login ➡️</button>
--- Client/src/pages/admin/Layout.jsx
+++ Client/src/pages/admin/Layout.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/admin/Sidebar';
 
 const Layout = () => {
+    const navigate=useNavigate();
+    const logout=()=>{
+        navigate('/');
+    }
+
   return (
-    <div>Layout</div>
+    <div>
+        <div className=' flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+            <div onClick={()=>navigate('/')} className="cursor-pointer text-4xl font-extrabold bg-gradient-to-r from-cyan-400 via-blue-400 to-indigo-500 text-transparent bg-clip-text">
+                DevBlogs
+            </div>            
+            <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
+        </div>
+
+        <div className='flex h-[calc(100vh-70px)]'>
+            <Sidebar/>
+            <Outlet/>
+        </div>
+    </div>
   )
 }
 
