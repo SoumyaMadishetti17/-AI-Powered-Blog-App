e60981a1ae7a764729b45091e4a53303fa579c6e
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2025-07-11 11:39:19 +0530

    stage-admin-dashboard layouts

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/blog/:id" element={<Blog/>}/>
-
+        <Route path='/admin' element={<Layout/>}
       </Routes>
     </div>
   )
