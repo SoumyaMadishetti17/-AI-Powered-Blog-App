75eaa95a776d9dcb96f9c7b0ebfe466e44c23d19
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2025-07-25 12:27:06 +0530

    stage backend

---
+++ Server/controllers/blogController.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
+export const addBlog = async (req, res) => {
+    try {
+        const {title, subTitle, description,  category, isPublished} = JSON.parse( req.body.blog );
+        const imageFile = req.file;
+
+        // check if all fields are present
+        if (!title || !description || !category || !imageFile ){
+            return res.json({ success: false, message: "Missing required fields" });
+        }
+        const fileBuffer =fs.readFileSync( imageFile.path );
+
+        // upload image to imagekit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/blogs',
+        })
+
+        // optimization through imagekit url transformation
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                { quality: 'auto'}, // auto compress image
+                {format: 'webp'}, // convert to modern webp format
+                { width: 1280 } // width resize to 1280px
+            ]
+        })
+        const image= optimizedImageUrl;
+
+        await Blog.create({title, subTitle, description, image, category, isPublished})
+        res.json({ success: true, message: "Blog added successfully" });
+
+    }catch (err) {
+        res.json({ success: false, message: err.message });
+    }
+}
